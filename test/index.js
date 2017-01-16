42fd54beac4151a2221ab4b24d567a5bc7bf09d3
Zach Allen <zallen@pension-consultants.com>
2017-01-16 12:35:05 -0600

    updated neural network test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var NeuralNow = require('../index.js');
 NeuralNow.get("sine", function (neuralNet) {
-  var input = [[0,1,0]]
+  var input = [[0],[3.14],[1]]
   var output = neuralNet.forward(input);
   console.log(output);
 });
