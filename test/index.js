94e2f29e7612d82c77be247a803d3a1975e03f71
Zach Allen <zallen@pension-consultants.com>
2017-01-26 22:06:20 -0600

    added funcitonality to save models to disk

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 var NeuralNow = require('../index');
 var NeuralNowUtils = require('neural-now-utils');
-NeuralNow.get('spam-classifier', function(net) {
-    var text = "wats up jim"; // set input values here
+NeuralNow.get('spam-classifier', function(neuralNet) {
+    var text = "Check out this crazy offer at notarealwebsite.com/malware"; // set input values here
     var input = NeuralNowUtils.Text.toBigramVector(text);
-    var output = net.forward(input);
+    var output = neuralNet.forward(input).w;
     console.log(output);
 });
